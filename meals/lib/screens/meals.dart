c8dddb2feeada3407e67396336ee12e45b6ea726
AhmedSocrates <ahmedkinglion@gmail.com>
2025-09-22 19:13:27 +0400

    Updated the navigation between categories and meals

--- meals/lib/screens/meals.dart
+++ meals/lib/screens/meals.dart
@@ -1,38 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
 import 'package:meals/models/meals.dart';
+import 'package:meals/widgets/meal_item.dart';
 
-class MealsScreen extends StatelessWidget{
+class MealsScreen extends StatelessWidget {
   MealsScreen({super.key, required this.title, required this.meals});
 
   final String title;
-  final List<Meal> meals; 
-
-  
+  final List<Meal> meals;
 
   @override
   Widget build(BuildContext context) {
-    Widget content = ListView.builder(
-        itemBuilder: (ctx,index) => Text(meals[index].title),
-      );
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            "Uh Sorry! .. It seems that there are no meals here",textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+          ),
+          SizedBox(height: 12),
+          Text(
+            "Search for other meals in other tabs",
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+          ),
+        ],
+      ),
+    );
 
-      if (meals.isEmpty){
-        Widget content = Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text("Uh Sorry! .. It seems that there are no meals here"),
-              SizedBox(height: 12),
-              Text("Search for other meals in other tabs")
-          ],));
-      }
-    
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: 
+        (ctx, index) => 
+        MealItem(
+          meal: meals[index],
+        ),
+      );
+    }
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: content
+      appBar: AppBar(title: Text(title)),
+      body: content,
     );
   }
 }
--- meals/lib/widgets/category_grid_item.dart
+++ meals/lib/widgets/category_grid_item.dart
@@ -1,31 +1,37 @@
 import 'package:meals/models/category.dart';
 import 'package:flutter/material.dart';
 
-class CategoryGridItem extends StatelessWidget{
-  const CategoryGridItem({super.key, required this.category});
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({
+    super.key,
+    required this.category,
+    required this.onSelectCategory,
+  });
   final Category category;
+  final void Function() onSelectCategory;
 
   @override
   Widget build(BuildContext context) {
-    return InkWell( 
-      onTap: () {},
+    return InkWell(
+      onTap: onSelectCategory,
       child: Container(
         padding: EdgeInsets.all(16),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(16),
-          gradient: LinearGradient(colors: [
-            category.color.withOpacity(0.55),
-            category.color.withOpacity(0.9)
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight
-         )
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.55),
+              category.color.withOpacity(0.9),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
         child: Text(
-          category.title, 
+          category.title,
           style: Theme.of(context).textTheme.titleLarge!.copyWith(
             color: Theme.of(context).colorScheme.onSurface,
-          ),  
+          ),
         ),
       ),
     );
