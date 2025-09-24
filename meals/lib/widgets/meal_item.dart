64fc442eebf85c44bbc68cdba79b790d90b96f59
AhmedSocrates <ahmedkinglion@gmail.com>
2025-09-24 19:46:23 +0400

    new updates in the meal item widget

--- meals/lib/widgets/meal_item.dart
+++ meals/lib/widgets/meal_item.dart
@@ -2,32 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:transparent_image/transparent_image.dart';
 import 'package:meals/widgets/meal_item_trait.dart';
 import 'package:meals/models/meals.dart';
-class MealItem extends StatelessWidget{
-  const MealItem({
-    super.key,
-    required this.meal
-  }); 
+
+class MealItem extends StatelessWidget {
+  const MealItem({super.key, required this.meal});
   final Meal meal;
+
+  String get complexityText {
+    return meal.complexity.name[0].toUpperCase() + meal.complexity.name.substring(1);
+  }
   @override
   Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.all(8),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8)
-      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
       clipBehavior: Clip.hardEdge,
       elevation: 2,
       child: InkWell(
         onTap: () {},
         child: Stack(
           children: [
-            FadeInImage(placeholder: MemoryImage(kTransparentImage),
-             image: NetworkImage(meal.imageUrl),
-             fit: BoxFit.cover,
-             height: 200,
-             width: double.infinity,
-             ),
-             Positioned(
+            FadeInImage(
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(meal.imageUrl),
+              fit: BoxFit.cover,
+              height: 200,
+              width: double.infinity,
+            ),
+            Positioned(
               bottom: 0,
               left: 0,
               right: 0,
@@ -36,25 +37,37 @@ class MealItem extends StatelessWidget{
                 color: Colors.black54,
                 child: Column(
                   children: [
-                    Text(meal.title,
-                    maxLines: 2,
-                    textAlign: TextAlign.center,
-                    softWrap: true,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white
+                    Text(
+                      meal.title,
+                      maxLines: 2,
+                      textAlign: TextAlign.center,
+                      softWrap: true,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
                     ),
+                    const SizedBox(height: 12),
+                    Row(
+                      children: [
+                        MealItemTrait(
+                          icon: Icons.schedule,
+                          label: '${meal.duration} mins',
+                        ),
+                        const SizedBox(width: 12),
+                        MealItemTrait(
+                          icon: Icons.work,
+                          label: '${complexityText} mins',
+                        )
+
+                      ],
                     ),
-                    const SizedBox(height: 12,),
-                    Row(children: [ 
-                      MealItemTrait(icon: Icons.schedule, label: '${meal.duration} mins')
-                    ],)
                   ],
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
